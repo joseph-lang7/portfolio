cede35684320dff4ed303c1a49e3e3dbae3badcb
joseph <langj6757@gmail.com>
2024-06-12 19:48:16 -0700

    Implement hero section

---
+++ src/constants/index.js
@@ -0,0 +1,2 @@
+export const HERO_TEXT =
+  "I am a passionate full stack developer from California. I have honed my skills in front-end technologies like React and Next.js, as well as back-end technologies like Node.js, MySQL, and MongoDB.";
